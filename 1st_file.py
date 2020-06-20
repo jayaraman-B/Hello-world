e58be5ee9de7ac5e388e88cba004decda94a1304
Jayaraman <62459532+jayaraman-B@users.noreply.github.com>
2020-06-20 16:15:54 +0530

    conflict solved

--- 1st_file.py
+++ 1st_file.py
@@ -2,8 +2,6 @@ print("Hello World ")
 print("SPYDY")
 print("Avengers")
 print("Justice League")
-<<<<<<< HEAD
 print("justice league-Super Man")
-=======
 print("Avengers-Iron Man)
->>>>>>> first
+
